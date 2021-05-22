779739225b27e39d5377a50ee15d7c54a22f6bad
Your Name <you@example.com>
2021-05-22 21:43:41 +0300

    Получение аргументов программы argc>1

--- main.cpp
+++ main.cpp
@@ -34,8 +34,11 @@ if(prompt)
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
     curl_global_init(CURL_GLOBAL_ALL);
+     if (argc>1)
+        for(int i=0; i<argc; i++)
+            cout<<"argv["<<i<<"] = "<<argv[i]<<endl;
     const auto input = read_input(cin,true);
     const auto bins = make_histogram(input);
     //show_histogram_text(bins);
