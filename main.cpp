6474e4a96e2e102e16815968ee050f11075cb12c
Your Name <you@example.com>
2021-05-22 21:21:05 +0300

    read_input(cin, true)

--- main.cpp
+++ main.cpp
@@ -16,16 +16,16 @@ input_numbers(istream& in,size_t count) {
 }
 
 Input
-read_input(istream& in) {
+read_input(istream& in,bool prompt) {
     Input data;
-
+if(prompt)
     cerr << "Enter number count: ";
     size_t number_count;
     in >> number_count;
-
+if(prompt)
     cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
-
+if(prompt)
      cerr << "Enter bin count: ";
      size_t bin_count;
      in >> data.bin_count;
@@ -34,7 +34,7 @@ read_input(istream& in) {
 
 
 int main() {
-    const auto input = read_input(cin);
+    const auto input = read_input(cin,true);
     const auto bins = make_histogram(input);
     //show_histogram_text(bins);
      show_histogram_svg(bins);
